d84a1f1421bd3c25cab63335da5370ed7adb250a
Ralf <ralf@pardatscher.net>
2022-01-21 18:10:58 +0100

    Upgraded API - Bug fixes - Endpoints for objects

--- restAPI/urls.py
+++ restAPI/urls.py
@@ -5,10 +5,12 @@ from restAPI import views
 
 urlpatterns = [
     path('measurementsSets/latest/', views.measurements_sets_latest),
+    path('measurementsSets/object/<int:id>', views.measurements_sets_object),
     path('measurementsSets/<int:pk>/', views.measurements_sets_id),
-    path('measurementsSets/<int:pk>/details/', views.measurements_sets_id),
+    path('measurementsSets/<int:pk>/details/', views.measurements_sets_id_details),
     path('measurementsSets/', views.measurements_sets),
     path('measurements/latest/', views.measurements_latest),
+    path('measurements/object/<int:id>', views.measurements_object),
     path('measurements/<int:pk>/', views.measurements_id),
     path('measurements/', views.measurements)
 ]
--- restAPI/views.py
+++ restAPI/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 
+from itertools import chain
+
 from restAPI.serializers import MeasurementsSetsSerializer, MeasurementsSerializer
 from restAPI.models import Measurements, MeasurementsSets
 
@@ -39,12 +41,23 @@ def measurements_sets_id_details(request, pk):
     basename = 'measurements-sets-id-details'
     try:
         queryset = Measurements.objects.all().filter(measurements_set=pk)
-    except (MeasurementsSets.DoesNotExist, Measurements.DoesNotExist):
+    except Measurements.DoesNotExist:
         return HttpResponse(status=404)
 
     if request.method == 'GET':
-        serializer = MeasurementsSerializer(queryset)
-        return JsonResponse(serializer.data)
+        serializer = MeasurementsSerializer(queryset, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+def measurements_sets_object(request, id):
+    basename = 'measurements-sets-object'
+    try:
+        queryset = MeasurementsSets.objects.all().filter(object_id=id)
+    except MeasurementsSets.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = MeasurementsSetsSerializer(queryset, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
 
 def measurements_sets(request):
@@ -69,11 +82,32 @@ def measurements_latest(request):
             complete=True)
         measurements_set_id_latest = measurements_set_id_latest.order_by(
             'timestamp')
-        measurements_set_id_latest = measurements_set_id_latest.first()
-        measurements_set_id_latest = measurements_set_id_latest.id
-        queryset = Measurements.objects.all()
-        queryset = queryset.filter(measurementsSet=measurements_set_id_latest)
-        queryset = queryset.order_by('timestamp')
+        measurements_set_id_latest = measurements_set_id_latest.filter(
+            timestamp=measurements_set_id_latest.first().timestamp)
+        queryset = MeasurementsSets.objects.none()
+        for set_id in measurements_set_id_latest:
+            queryset = chain(queryset, Measurements.objects.all().filter(measurements_set=set_id.id))
+    except (MeasurementsSets.DoesNotExist, Measurements.DoesNotExist):
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = MeasurementsSerializer(queryset, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    return HttpResponse(status=405)
+
+
+def measurements_object(request, id):
+    basename = 'measurements-object'
+    try:
+        measurements_set_id_latest = MeasurementsSets.objects.all()
+        measurements_set_id_latest = measurements_set_id_latest.order_by(
+            'timestamp')
+        measurements_set_id_latest = measurements_set_id_latest.filter(
+            object_id=id)
+        queryset = MeasurementsSets.objects.none()
+        for set_id in measurements_set_id_latest:
+            queryset = chain(queryset, Measurements.objects.all().filter(measurements_set=set_id.id))
     except (MeasurementsSets.DoesNotExist, Measurements.DoesNotExist):
         return HttpResponse(status=404)
 
